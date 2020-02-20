459d25fd9b3d728f0ef351613f855150dd299757
Jason Maurer <jason@maur.co>
2020-02-19 19:24:56 -0700

    Moving to async await

--- shouts/shouts-router.js
+++ shouts/shouts-router.js
@@ -3,22 +3,39 @@ const shoutsModel = require("./shouts-model")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
-	shoutsModel.find()
-		.then((data) => res.status(200).json(data))
-		.catch((err) => next(err))
+// **** USING PROMISE CHAINS INSTEAD OF ASYNC/AWAIT
+// router.get("/", (req, res, next) => {
+// 	shoutsModel.find()
+// 		.then((data) => res.status(200).json(data))
+// 		.catch((err) => next(err))
+// })
+
+router.get("/", async (req, res, next) => {
+	try {
+		// `shoutsModel.find` returns a promise, so we can "await" it rather than use .then
+		const data = await shoutsModel.find()
+		res.status(200).json(data)
+	} catch(err) {
+		next(err)
+	}
 })
 
-router.get("/:id", (req, res, next) => {
-	shoutsModel.findById(req.params.id)
-		.then((data) => res.status(200).json(data))
-		.catch((err) => next(err))
+router.get("/:id", async (req, res, next) => {
+	try {
+		const data = await shoutsModel.findById(req.params.id)
+		res.status(200).json(data)
+	} catch(err) {
+		next(err)
+	}
 })
 
-router.post("/", (req, res, next) => {
-	shoutsModel.add(req.body)
-		.then((data) => res.status(201).json(data))
-		.catch((err) => next(err))
+router.post("/", async (req, res, next) => {
+	try {
+		const data = await shoutsModel.add(req.body)
+		res.status(201).json(data)
+	} catch(err) {
+		next(err)
+	}
 })
 
 module.exports = router
