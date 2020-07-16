888d0046f5ea1abe920ec0b4d4e1897bfefcd40a
yirano <yirano@protonmail.ch>
2020-07-15 23:15:42 -0400

    Add morgan

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const helmet = require("helmet")
 const cors = require("cors")
+const morgan = require("morgan")
 const welcomeRouter = require("./welcome/welcome-router")
 const shoutsRouter = require("./shouts/shouts-router")
 
 const server = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000 // added something here
 
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+server.use(morgan('combined'))
 
 server.use("/", welcomeRouter)
 server.use("/shouts", shoutsRouter)
